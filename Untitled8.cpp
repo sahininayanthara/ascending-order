4e09cfefff72bc9e1b528823c47922002c457b86
sahininayanthara <sahininayanthara95@gmail.com>
2019-02-19 23:13:29 +0530

    first commit

---
+++ Untitled8.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+int array[100],n,c,d,t;
+	
+	printf("Enter number of elements\n");
+	scanf("%d",&n);
+	
+	printf("Enter %d integers\n",n);
+	
+	for(c=0;c<n;c++){
+	scanf("%d",&array[c]);
+	}
+	for(c=1;c<=n-1;c++){
+	d=c;
+	while(d>0&&array[d-1]>array[d]){
+		t=array[d];
+		array[d]=array[d-1];
+		array[d-1]=t;
+		d--;
+	}
+	 }
+	 printf("sorted list in ascending order\n");
+	for(c=0;c<=n-1;c++){
+		printf("%d\n",array[c]);
+	}
+	return 0;
+}
